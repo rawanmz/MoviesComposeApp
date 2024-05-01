3153285b79247550baf7fcea88b3f8f4cc8cffb6
rawanalzidan <rwan14192010@gmail.com>
2024-05-01 18:53:21 +0300

    -add onBoardingScreen

---
+++ app/src/main/java/com/example/moviescomposeapp/presentation/screens/onBoarding/OnBoardingScreen.kt
@@ -0,0 +1,277 @@
+package com.example.moviescomposeapp.presentation.screens.onBoarding
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.moviescomposeapp.R
+import com.example.moviescomposeapp.presentation.navigation.Screens
+import com.example.moviescomposeapp.ui.theme.DarkGreenBlue
+import com.example.moviescomposeapp.ui.theme.LightYellow
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun OnBoardingScreen(navController: NavHostController) {
+    val pagerState = rememberPagerState(
+        0,
+        pageCount = {
+            3
+        })
+    HorizontalPager(state = pagerState) { page ->
+        Column {
+            Row(
+                Modifier
+                    .height(50.dp)
+                    .fillMaxWidth()
+                    .background(DarkGreenBlue)
+                    .padding(top = 16.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                (0..2).forEach { index ->
+                    val color =
+                        if (pagerState.currentPage == index) LightYellow else Color.White
+                    Box(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .background(color, CircleShape)
+                            .size(10.dp)
+                    )
+                }
+            }
+            when (page) {
+                0 -> {
+                    FirstScreen()
+                }
+
+                1 -> {
+                    SecondScreen()
+                }
+
+                2 -> {
+                    ThirdScreen(navController)
+                }
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun FirstScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DarkGreenBlue),
+        contentAlignment = Alignment.BottomCenter
+    )
+    {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(10.dp)
+                .rotate(-90f)
+                .paint(
+                    // Replace with your image id
+                    painterResource(id = R.drawable.bottom_background),
+                    contentScale = ContentScale.FillWidth,
+                    sizeToIntrinsics = false
+                )
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.movie),
+                contentDescription = "fil image",
+                modifier = Modifier.size(350.dp)
+            )
+
+            Text(
+                text = "Welcome to CineSpectra!",
+                modifier = Modifier.padding(12.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = "Explore the latest movies, reserve the perfect seats, and experience the cinema in a whole new way.",
+                modifier = Modifier.padding(12.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp,
+                color = Color.White
+            )
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SecondScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DarkGreenBlue),
+        contentAlignment = Alignment.BottomCenter
+    )
+    {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(10.dp)
+                .rotate(-90f)
+                .paint(
+                    // Replace with your image id
+                    painterResource(id = R.drawable.bottom_background),
+                    contentScale = ContentScale.FillWidth,
+                    sizeToIntrinsics = false
+                )
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.home_cinema),
+                contentDescription = "fil image",
+                modifier = Modifier.size(350.dp)
+            )
+
+            Text(
+                text = "Welcome to CineSpectra!",
+                modifier = Modifier.padding(12.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = "Explore the latest movies, reserve the perfect seats, and experience the cinema in a whole new way.",
+                modifier = Modifier.padding(12.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp,
+                color = Color.White
+            )
+        }
+    }
+}
+
+
+@Composable
+fun ThirdScreen(navController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DarkGreenBlue),
+        contentAlignment = Alignment.BottomCenter
+    )
+    {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(10.dp)
+                .rotate(-90f)
+                .paint(
+                    // Replace with your image id
+                    painterResource(id = R.drawable.bottom_background),
+                    contentScale = ContentScale.FillWidth,
+                    sizeToIntrinsics = false
+                )
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.horror_movie),
+                contentDescription = "fil image",
+                modifier = Modifier.size(350.dp)
+            )
+
+            Text(
+                text = "Welcome to CineSpectra!",
+                modifier = Modifier.padding(12.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Text(
+                text = "Explore the latest movies, reserve the perfect seats, and experience the cinema in a whole new way.",
+                modifier = Modifier.padding(12.dp),
+                textAlign = TextAlign.Center,
+                fontSize = 16.sp,
+                color = Color.White
+            )
+            Row(
+                modifier = Modifier
+                    .padding(40.dp)
+                    .fillMaxHeight(),
+                verticalAlignment = Alignment.Bottom,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                AnimatedVisibility(
+                    modifier = Modifier.fillMaxWidth(),
+                    visible = true
+                ) {
+                    Button(
+                        onClick = { navController.navigate(Screens.PopularMovie.route) },
+                        colors = ButtonDefaults.buttonColors(
+                            contentColor = Color.White
+                        ),
+                        shape= RoundedCornerShape(5.dp)
+                    ) {
+                        Text(text = "Finish", modifier = Modifier.padding(4.dp))
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/moviescomposeapp/presentation/screens/popular/PopularMoviesScreen.kt
@@ -0,0 +1,105 @@
+package com.example.moviescomposeapp.presentation.screens.popular
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.example.moviescomposeapp.Constant.MOVIE_IMAGE_BASE_URL
+import com.example.moviescomposeapp.R
+import com.example.moviescomposeapp.model.BackdropSize
+import com.example.moviescomposeapp.model.SearchResponse
+import com.example.moviescomposeapp.model.UIState
+import com.example.moviescomposeapp.ui.theme.GreenBlue
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun PopularMoviesScreen(
+    navController: NavHostController,
+    popularMoviesState: MutableState<UIState<SearchResponse>>
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(GreenBlue)
+    ) {
+        when (val result = popularMoviesState.value) {
+            is UIState.Success -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(GreenBlue),
+                    contentAlignment = Alignment.TopCenter
+                )
+                {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(0.4f)
+                            .background(GreenBlue)
+                            .rotate(-90f)
+                            .paint(
+                                // Replace with your image id
+                                painterResource(id = R.drawable.background),
+                                contentScale = ContentScale.FillWidth,
+                                sizeToIntrinsics = false
+                            )
+                    )
+
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        item {
+                            Image(
+                                painter = painterResource(id = R.drawable.horror_movie),
+                                contentDescription = "fil image",
+                                modifier = Modifier.size(350.dp)
+                            )
+
+                        }
+                        items(result.data?.results.orEmpty()) {
+                            GlideImage(
+                                model = "${MOVIE_IMAGE_BASE_URL}${BackdropSize.w300}/${it.posterPath}",
+                                contentDescription = "",
+                                modifier = Modifier
+                                    .padding(12.dp)
+                                    .fillParentMaxWidth(),
+                            )
+
+                            Text(
+                                text = it.title.orEmpty(),
+                                modifier = Modifier.padding(12.dp)
+                            )
+                        }
+                    }
+                }
+
+            }
+
+            is UIState.Empty -> {}
+            is UIState.Error -> {}
+            is UIState.Loading -> {}
+
+        }
+    }
+}
